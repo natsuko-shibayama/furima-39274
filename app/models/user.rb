948219c6a32332cedc8e701e43e6f3e3ec890bf4
natsuko-shibayama <124324097+natsuko-shibayama@users.noreply.github.com>
2023-05-17 19:29:52 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true
   validates :nickname, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
