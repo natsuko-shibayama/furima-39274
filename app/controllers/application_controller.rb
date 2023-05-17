eca213bfd2179f8e3c3894e00941bcbe16959016
natsuko-shibayama <124324097+natsuko-shibayama@users.noreply.github.com>
2023-05-17 17:25:48 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,4 +10,9 @@ class ApplicationController < ActionController::Base
       username == 'admin' && password == '2222'
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :password, :email, :first_name, :last_name, :first_name_kana, :last_name_kana, :date_of_birth])
+  end
+
 end
