9bb36d0e38036d8cb1765ca3ea9da4a11f9b2f82
natsuko-shibayama <124324097+natsuko-shibayama@users.noreply.github.com>
2023-05-18 17:17:22 +0900

    userモデルのテストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :nickname, presence: true, length: { maximum: 40 }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "is invalid" }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "is invalid" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid" }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid" }
   validates :date_of_birth, presence: true
 end
