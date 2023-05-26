68232c891121107cdfb6fadf32c376666e4fd5b6
natsuko-shibayama <124324097+natsuko-shibayama@users.noreply.github.com>
2023-05-26 18:09:01 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_name, class_name: 'Category'
+  belongs_to :condition
+  belongs_to :shipping_fee_payer
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
   belongs_to :user
   has_one_attached :image
 end
