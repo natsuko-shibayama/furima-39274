97dfcf90d92b74282a72fa1ae3e9d81b19b7fedf
natsuko-shibayama <124324097+natsuko-shibayama@users.noreply.github.com>
2023-05-26 18:22:52 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,15 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :category_name_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_fee_payer_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :price, presence: true,
+                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                    format: { with: /\A\d+\z/, message: "半角数値のみ入力してください" }
 end
