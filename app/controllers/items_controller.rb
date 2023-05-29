13d04d61a16f3a3e961241cb4672948049233376
natsuko-shibayama <124324097+natsuko-shibayama@users.noreply.github.com>
2023-05-29 12:01:04 +0900

    商品情報の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,27 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
   end
 
   def new
     @item = Item.new
   end
-end
 
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:content, :image, :name, :category_name_id, :condition_id, :shipping_fee_payer_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
+end
