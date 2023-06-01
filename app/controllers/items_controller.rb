f9a0e2e4f838b750f61657b44023f96c047b8a9e
natsuko-shibayama <124324097+natsuko-shibayama@users.noreply.github.com>
2023-06-01 17:30:28 +0900

    updateアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create]
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -25,12 +25,18 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to action: :show
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
 end
 
   private
