4f3a7e862f7a233e3c7c83de8ab81bb0ee2a912c
natsuko-shibayama <124324097+natsuko-shibayama@users.noreply.github.com>
2023-06-03 17:08:20 +0900

    モデル作成・バリデーション記載

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -2,14 +2,15 @@ class OrderForm
   include ActiveModel::Model
   attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number
 
-  validates :item_id, presence: true
-  validates :user_id, presence: true
-  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :prefecture_id, presence: true, numericality: {other_than: 0, message: "can't be blank"}
-  validates :city, presence: true
-  validates :street_address, presence: true
-  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
-
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :city
+    validates :street_address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
+  end
   def save
     Order.create(item_id: params[:item_id].to_i, user_id: current_user.id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_address: street_address, building_name: building_name, phone_number: phone_number, order_id: order.id)
