e7641007e1c8c3140fadc26fd53cb02800793bb0
natsuko-shibayama <124324097+natsuko-shibayama@users.noreply.github.com>
2023-06-04 15:57:36 +0900

    トークンをサーバーサイドに送る

---
+++ app/javascript/card.js
@@ -0,0 +1,28 @@
+const pay = () => {
+  const payjp = Payjp('pk_test_6875a1ab0e5dff480f182724');
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+        console.log(response.error.message);
+      } else {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
