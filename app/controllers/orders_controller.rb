c99b214690c4785b38844c9786b6cc204696010d
natsuko-shibayama <124324097+natsuko-shibayama@users.noreply.github.com>
2023-06-04 21:59:51 +0900

    購入画面修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,9 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id || @item.order
+      redirect_to root_path
+    end
     @orderform = OrderForm.new
   end
 
