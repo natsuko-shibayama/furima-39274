3864692895fcf845058da837b4376453574ca4e5
natsuko-shibayama <124324097+natsuko-shibayama@users.noreply.github.com>
2023-06-06 10:44:24 +0900

    prefectureのバリデーション修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -6,7 +6,7 @@ class OrderForm
     validates :item_id
     validates :user_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :prefecture_id, numericality: {only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 48, message: "can't be blank"}
     validates :city
     validates :street_address
     validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Please enter numbers only"}
